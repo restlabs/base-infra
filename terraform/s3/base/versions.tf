25cf280a7e390f73e5a5ed860049347c7f574a41
pafable <39135798+pafable@users.noreply.github.com>
2023-09-14 15:56:11 -0400

    Update versions.tf

--- terraform/s3/base/versions.tf
+++ terraform/s3/base/versions.tf
@@ -1,11 +1,5 @@
-terraform {
-  backend "s3" {}
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+module "terraform_aws_version" {
+  source = ../../modules/terraform_version
 }
 
 provider "aws" {
