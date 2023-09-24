c11d7991bfb82750398bee9f8f021a4827f10ce1
pafable <philafable@gmail.com>
2023-09-24 13:44:43 -0400

    fixed cookbook

--- cookbooks/base-ami/recipes/install_apache.rb
+++ cookbooks/base-ami/recipes/install_apache.rb
@@ -12,7 +12,7 @@ apt_package 'Install Apache' do
 end
 
 file '/var/www/html/index.html' do
-  content "This is configured by Chef.\n\n
+  content "This is configured by Chef.
   DO NOT EDIT THIS MANUALLY!\n"
   action :create
 end
