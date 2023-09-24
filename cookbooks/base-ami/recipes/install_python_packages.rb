92c53d9e4b2c3095d00c9c14969db22fcb3e2aaf
pafable <philafable@gmail.com>
2023-09-24 14:49:31 -0400

    fixed cookbook

--- cookbooks/base-ami/recipes/install_python_packages.rb
+++ cookbooks/base-ami/recipes/install_python_packages.rb
@@ -10,12 +10,16 @@ execute 'install_pip3' do
   command "curl #{PIP_INSTALLER_URL} > get-pip.py && sudo python get-pip.py && rm -f get-pip.py"
 end
 
+execute 'update_pip' do
+  command 'python -m pip install --upgrade pip'
+end
+
 [
   'pip',
   'setuptools',
   'wheel',
 ].each do | package_name |
   execute 'install_python_packages' do
-    command "python -m install pip #{package_name}"
+    command "python -m pip install #{package_name}"
   end
 end
