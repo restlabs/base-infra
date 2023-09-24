9fc34ad73ef98554a5776bc7d54ed5e02026d9ae
pafable <philafable@gmail.com>
2023-09-24 14:12:07 -0400

    fixed cookbook

---
+++ cookbooks/base-ami/recipes/install_python.rb
@@ -0,0 +1,13 @@
+python 'python3' do
+  command "get-pip.py"
+end
+
+[
+  'pip'
+  'setuptools'
+  'wheel'
+].each do package_name |
+  python 'python3' do
+    command "-m install pip #{package_name}"
+  end
+end
