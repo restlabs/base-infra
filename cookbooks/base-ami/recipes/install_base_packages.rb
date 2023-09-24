f7b29c2cf06cf428d1ae5fe19969e56801450b8a
pafable <philafable@gmail.com>
2023-09-24 09:08:07 -0400

    fixed cookbook

--- cookbooks/base-ami/recipes/install_base_packages.rb
+++ cookbooks/base-ami/recipes/install_base_packages.rb
@@ -8,6 +8,20 @@
   'tmux',
   'unzip',
   'vim',
+  'watch',
 ].each do | package_name |
   apt_package package_name
+end
+
+execute 'check_platform' do
+  command "echo #{node[:platform]}"
+end
+
+apt_package 'Install Apache' do
+  case node[:platform]
+  when 'amazon', 'centos', 'redhat'
+    package_name 'httpd'
+  when 'debian', 'ubuntu'
+    package_name 'apache2'
+   end
 end
