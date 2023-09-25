9ac65e1cd7eaa4182c480296b08ea3801e5ac068
pafable <philafable@gmail.com>
2023-09-25 00:20:15 -0400

    created test

--- cookbooks/base-ami/recipes/install_base_packages.rb
+++ cookbooks/base-ami/recipes/install_base_packages.rb
@@ -1,21 +1,19 @@
-# execute 'install_epel' do
-#   command 'amazon-linux-extras install epel'
-# end
+execute 'install_epel' do
+  command 'amazon-linux-extras install epel'
+end
 
-# [
-#   'git',
-#   'htop',
-#   'jq',
-#   'lynx',
-#   'net-tools',
-#   'screenfetch',
-#   'tmux',
-#   'unzip',
-#   'vim',
-#   'watch',
-# ].each do | package_name |
-#
-#   yum_package package_name
-# end
+[
+  'git',
+  'htop',
+  'jq',
+  'lynx',
+  'net-tools',
+  'screenfetch',
+  'tmux',
+  'unzip',
+  'vim',
+  'watch',
+].each do | package_name |
 
-yum_package 'git'
+  package package_name
+end
