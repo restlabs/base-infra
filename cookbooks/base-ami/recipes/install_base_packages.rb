ee8404f9b7d19687a3f86e9fc82af27c1d7aa9b1
pafable <philafable@gmail.com>
2023-09-25 00:44:41 -0400

    created test

--- cookbooks/base-ami/recipes/install_base_packages.rb
+++ cookbooks/base-ami/recipes/install_base_packages.rb
@@ -1,10 +1,26 @@
 execute 'install_epel' do
+  command 'whoami'
+end
+
+execute 'install_epel' do
   command 'amazon-linux-extras install epel'
 end
 
-execute 'set_locale' do
-  command 'export LC_ALL=en_US.UTF-8'
-end
+# [
+#   'git',
+#   'htop',
+#   'jq',
+#   'lynx',
+#   'net-tools',
+#   'tmux',
+#   'unzip',
+#   'vim',
+#   'watch',
+# ].each do | package_name |
+#   execute 'install_packages' do
+#     command "yum install -y #{package_name}"
+#   end
+# end
 
 [
   'git',
@@ -12,12 +28,10 @@ end
   'jq',
   'lynx',
   'net-tools',
-  'screenfetch',
   'tmux',
   'unzip',
   'vim',
   'watch',
 ].each do | package_name |
-
   package package_name
 end
