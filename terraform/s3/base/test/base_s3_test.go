cf49047d9476c204b2e307c298bfbaeda69ba278
pafable <philafable@gmail.com>
2023-09-27 15:40:25 -0400

    added vars file

--- terraform/s3/base/test/base_s3_test.go
+++ terraform/s3/base/test/base_s3_test.go
@@ -9,6 +9,20 @@ import (
 func TestTerraformBaseS3(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../",
+		BackendConfig: map[string]interface{}{
+			"bucket":         "pafable-tfstates-100",
+			"key":            "vpc/base/terraform.tfstate",
+			"region":         "us-east-1",
+			"encrypt":        "true",
+			"dynamodb_table": "terraform-lock",
+		},
+		Reconfigure: true,
+		Vars: map[string]interface{}{
+			"email":  "pafable@test.com",
+			"owner":  "pafable",
+			"region": "us-east-1",
+		},
+		VarFiles: []string{"terrat-test.tfvars"},
 	})
 
 	// destroy resources when test ends successfully or not
@@ -18,7 +32,7 @@ func TestTerraformBaseS3(t *testing.T) {
 
 	// checks if bucket name is base-infra-1-bucket-test-us-east-1
 	outputBucketName := terraform.Output(t, terraformOptions, "bucket_name")
-	assert.Equal(t, "base-infra-1-bucket-test-us-east-1", outputBucketName)
+	assert.Equal(t, "base-infra-1-bucket-dev-us-east-1", outputBucketName)
 
 	// checks if region is us-east-1
 	outputBucketRegion := terraform.Output(t, terraformOptions, "bucket_region")
