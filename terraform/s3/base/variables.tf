675547126f766aa2aaca65005a7f7101c051542f
pafable <39135798+pafable@users.noreply.github.com>
2023-09-30 16:30:35 -0400

    Feature/enable test (#19)
    * enabled

--- terraform/s3/base/variables.tf
+++ terraform/s3/base/variables.tf
@@ -1,10 +1,15 @@
 variable "app_name" {
   type    = string
-  default = "base-infra-1"
+  default = "base-infra"
 }
 
 variable "email" {}
 
 variable "owner" {}
+
+variable "params_region" {
+  type    = string
+  default = "us-east-1"
+}
 
 variable "region" {}
