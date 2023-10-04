d400bfd55b074c9a0afb102dabfdeb3e1cd99644
pafable <philafable@gmail.com>
2023-10-04 12:17:25 -0400

    switched source to "terraform-aws-modules/eks/aws"

--- terraform/eks/base/main.tf
+++ terraform/eks/base/main.tf
@@ -1,13 +1,27 @@
+locals {
+  ami_type         = "AL2_x86_64"
+  cluster_name     = "${var.owner}-${var.environment}-eks-${var.region}"
+  desired_capacity = 3
+  max_capacity     = 4
+  min_capacity     = 2
+  instance_types   = ["t3.small"]
+}
+
+data "aws_vpc" "selected" {
+  default = true
+}
+
 module "base_eks" {
-  source                      = "../../modules/eks"
-  code_location               = "terraform/eks/base"
-  eks_nodegroup_instance_type = var.eks_nodegroup_instance_type
-  eks_version                 = 1.28
-  email                       = var.email
-  environment                 = var.environment
-  isDefaultVpc                = true
-  owner                       = var.owner
-  project                     = "base-infra"
-  region                      = var.region
-  vpc_id                      = null
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.16.0"
+  cluster_name    = local.cluster_name
+  cluster_version = 1.28
+  vpc_id          = data.aws_vpc.selected.id
+  eks_managed_node_group_defaults = {
+    ami_type         = local.ami_type
+    desired_capacity = local.desired_capacity
+    max_capacity     = local.max_capacity
+    min_capacity     = local.min_capacity
+    instance_types   = local.instance_types
+  }
 }
--- terraform/modules/eks/kubeconfig.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "connect_eks_kube" {
-  provisioner "local-exec" {
-    command = "aws eks --region ${var.region} update-kubeconfig --name ${local.cluster_name}"
-  }
-
-  depends_on = [
-    aws_eks_cluster.my_cluster
-  ]
-}
