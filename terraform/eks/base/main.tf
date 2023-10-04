6c1c6c6816bcd740aea7d8420dd524046d5600d0
pafable <philafable@gmail.com>
2023-10-04 19:10:06 -0400

    fixed vpc issues for eks

--- terraform/eks/base/main.tf
+++ terraform/eks/base/main.tf
@@ -1,6 +1,7 @@
 locals {
   ami_type         = "AL2_x86_64"
   cluster_name     = "${var.owner}-${var.environment}-eks-${var.region}"
+  disk_size        = 50
   desired_capacity = 3
   max_capacity     = 4
   min_capacity     = 2
@@ -34,12 +35,17 @@ module "base_eks" {
   cluster_version = 1.28
   # cluster_endpoint_public_access_cidrs = [] # set this when going to prod
   control_plane_subnet_ids = data.aws_subnets.tf_subnet.ids
+  subnet_ids               = data.aws_subnets.tf_subnet.ids
   vpc_id                   = data.aws_vpc.selected.id
+
   eks_managed_node_group_defaults = {
     ami_type         = local.ami_type
+    disk_size        = local.disk_size
+    instance_types   = local.instance_types
     desired_capacity = local.desired_capacity
     max_capacity     = local.max_capacity
     min_capacity     = local.min_capacity
-    instance_types   = local.instance_types
   }
+
+  eks_managed_node_groups = {}
 }
