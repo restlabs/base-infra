8f67a8d11bc8213139d3439941e860f0729b8b28
pafable <philafable@gmail.com>
2023-10-05 00:26:48 -0400

    fixed access

--- terraform/s3/base/main.tf
+++ terraform/s3/base/main.tf
@@ -1,10 +1,11 @@
 module "base_s3_bucket" {
-  source        = "../../modules/s3"
-  app_name      = var.app_name
-  code_location = "terraform/s3/base"
-  email         = var.email
-  owner         = var.owner
-  region        = var.region
-  environment   = data.aws_ssm_parameter.account_env.value
-  project       = "base-infra"
+  source           = "../../modules/s3"
+  app_name         = var.app_name
+  block_public_acl = true
+  code_location    = "terraform/s3/base"
+  email            = var.email
+  owner            = var.owner
+  region           = var.region
+  environment      = data.aws_ssm_parameter.account_env.value
+  project          = "base-infra"
 }
