63868388f1d58e83cc8a571b53a4862cf724fba0
pafable <39135798+pafable@users.noreply.github.com>
2023-10-05 21:29:11 -0400

    igw mod (#45)

---
+++ terraform/modules/internet_gateway/variables.tf
@@ -0,0 +1,39 @@
+variable "app_name" {
+  description = "app name"
+  type        = string
+}
+
+variable "code_location" {
+  description = "code location"
+  type        = string
+}
+
+variable "email" {
+  description = "email"
+  type        = string
+}
+
+variable "environment" {
+  description = "environment"
+  type        = string
+}
+
+variable "owner" {
+  description = "owner"
+  type        = string
+}
+
+variable "project" {
+  description = "project"
+  type        = string
+}
+
+variable "region" {
+  description = "region"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "vpc id"
+  type        = string
+}
---
+++ terraform/modules/internet_gateway/version.tf
@@ -0,0 +1,22 @@
+module "terraform_aws_version" {
+  source = "../terraform_version"
+}
+
+locals {
+  common_tags = {
+    # keep Name key capitalized so that it will be set in the resource name!
+    Name          = var.app_name
+    code_location = var.code_location
+    email         = var.email
+    environment   = var.environment
+    owner         = var.owner
+    project       = var.project
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = local.common_tags
+  }
+  region = var.region
+}
