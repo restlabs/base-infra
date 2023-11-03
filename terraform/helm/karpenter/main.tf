9c8f1f39d263f0e64db387f4efc226d76872593d
pafable <philafable@gmail.com>
2023-11-02 20:34:50 -0400

    add karpenter

---
+++ terraform/helm/karpenter/main.tf
@@ -0,0 +1,16 @@
+locals {
+  cluster_name         = "${var.owner}-${local.base_tags.environment}-eks-${var.region}"
+  karpenter_chart      = "karpenter"
+  karpenter_version    = "v0.32.1"
+  karpenter_namespace  = "karpenter"
+  karpenter_repository = "oci://public.ecr.aws/karpenter/karpenter"
+}
+
+module "karpenter" {
+  source        = "../../modules/helm-install"
+  chart         = local.karpenter_chart
+  chart_version = local.karpenter_version
+  namespace     = local.karpenter_namespace
+  release_name  = local.karpenter_chart
+  repository    = local.karpenter_repository
+}
