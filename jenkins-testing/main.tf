675463def178e69ddadedaeef8c67113f76af41c
pafable <philafable@gmail.com>
2024-01-21 12:52:20 -0500

    created manifest for jenkins testing

--- jenkins-testing/main.tf
+++ jenkins-testing/main.tf
@@ -1,22 +1,3 @@
-module "kubernetes" {
-  source = "../terraform/modules/kubernetes-version"
-}
-
-module "terraform_aws_version" {
-  source = "../terraform/modules/terraform-aws-version"
-}
-
-provider "aws" {
-  alias  = "parameters"
-  region = "us-east-1"
-}
-
-provider "kubernetes" {
-  host                   = data.aws_ssm_parameter.eks_cluster_endpoint.value
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
-}
-
 locals {
   cluster_name = "${var.owner}-${local.base_tags.environment}-eks-${var.region}"
   base_tags = {
@@ -26,47 +7,6 @@ locals {
     owner         = var.owner
     project       = "base-infra"
   }
-}
-
-variable "email" {
-  type        = string
-  description = "email"
-  default     = "pafable@test"
-}
-
-variable "owner" {
-  type        = string
-  description = "owner"
-  default     = "pafable"
-}
-
-variable "region" {
-  type        = string
-  description = "region"
-  default     = "us-east-2"
-}
-
-data "aws_ssm_parameter" "eks_cluster_name" {
-  provider = aws.parameters
-  name     = "/eks/base/id"
-}
-
-data "aws_ssm_parameter" "eks_cluster_endpoint" {
-  provider = aws.parameters
-  name     = "/eks/base/endpoint"
-}
-
-data "aws_eks_cluster" "eks_cluster" {
-  name = local.cluster_name
-}
-
-data "aws_eks_cluster_auth" "eks_cluster" {
-  name = local.cluster_name
-}
-
-data "aws_ssm_parameter" "account_env" {
-  provider = aws.parameters
-  name     = "/account/environment"
 }
 
 resource "kubernetes_namespace_v1" "example_ns" {
---
+++ jenkins-testing/variables.tf
@@ -0,0 +1,17 @@
+variable "email" {
+  type        = string
+  description = "email"
+  default     = "pafable@test"
+}
+
+variable "owner" {
+  type        = string
+  description = "owner"
+  default     = "pafable"
+}
+
+variable "region" {
+  type        = string
+  description = "region"
+  default     = "us-east-2"
+}
