487a359280a14b0f137bcb8fc8287e554890a453
pafable <philafable@gmail.com>
2024-02-02 22:12:47 -0500

    test consul

--- terraform/eks/base/karpenter.tf
+++ terraform/eks/base/karpenter.tf
@@ -25,7 +25,7 @@ resource "helm_release" "karpenter" {
   repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
   version             = "v0.32.6"
-  timeout             = 1800
+  timeout             = 800
 
   values = [
     <<-EOT
