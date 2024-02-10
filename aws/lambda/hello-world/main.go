055bf6f372c7f53e8f262245f63ad26a0b6ab9b6
pafable <philafable@gmail.com>
2024-02-10 10:29:16 -0500

    test go lambda

---
+++ aws/lambda/hello-world/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello, World!",
+		StatusCode: 200,
+	}, nil
+}
