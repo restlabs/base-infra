28655a87d19e8125de116dcd436dc1b856a0da0e
pafable <philafable@gmail.com>
2024-02-11 19:46:38 -0500

    test go lambda

--- terraform/lambda/hello-world/main.tf
+++ terraform/lambda/hello-world/main.tf
@@ -32,8 +32,6 @@ resource "null_resource" "package_file" {
 }
 
 resource "aws_lambda_function" "tf_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
   filename      = "${local.code_dir}/main.zip"
   function_name = local.function_name
   role          = aws_iam_role.iam_for_lambda.arn
